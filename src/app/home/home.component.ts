3c184c7ce676cc0f8451776c83928313798adcd1
Edward <edward.neto@triersistemas.com.br>
2022-06-22 08:05:00 -0300

    correcao

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,59 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {}
 
   validar(): void {
-    const valor = this.cpf;
+    if (this.isCpfValid(this.cpf)) {
+      this.cpf = this.format(this.cpf);
+    } else {
+      this.cpf = 'Inválido!';
+    }
+  }
 
-    this.cpf = 'Inválido.';
+  private format(value: string): string {
+    const val = this.extractNumbers(value);
+    if (val.length === 11) {
+      return val.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+    } else if (val.length === 14) {
+      return val.replace(
+        /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,
+        '$1.$2.$3/$4-$5'
+      );
+    }
+    return val;
+  }
+
+  private isCpfValid(cpf: string): boolean {
+    const digits = this.extractNumbersToList(cpf);
+    if (digits.length == 11 && this.distinct(digits).length > 1) {
+      return this.getCpfValid(digits.splice(0, 9)) === this.extractNumbers(cpf);
+    }
+    return false;
+  }
+
+  private getCpfValid(digits: number[]): string {
+    digits.push(this.mod11(digits, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    digits.push(this.mod11(digits, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    return digits.join('');
+  }
+
+  private mod11(digits: number[], multipliers: number[]): number {
+    let i = 0;
+    const rest = digits.reduce((p, e) => p + e * multipliers[i++], 0) % 11;
+    return rest > 9 ? 0 : rest;
+  }
+
+  private distinct(digits: number[]): number[] {
+    return [...new Set(digits.map((item) => item))];
+  }
+
+  private extractNumbersToList(value: string): number[] {
+    const digits: number[] = [];
+    for (const item of this.extractNumbers(value).split('')) {
+      digits.push(parseInt(item));
+    }
+    return digits;
+  }
+
+  private extractNumbers(s: string): string {
+    return s ? s.replace(/\D+/g, '') : '';
   }
 }
