e8e6fd82ea6f6afccb7e6af9194e6bff8eab7e6b
Edward <edward.neto@triersistemas.com.br>
2022-06-27 08:44:50 -0300

    editar

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -19,6 +19,7 @@ export class ClienteComponent implements OnInit {
   list: Cliente[] = [];
 
   form: FormGroup = this.formBuilder.group({
+    id: new FormControl(null),
     nome: new FormControl(null, [Validators.required, Validators.minLength(2)]),
     cpf: new FormControl(null, [Validators.required, Validators.minLength(11)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
@@ -28,6 +29,10 @@ export class ClienteComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.carregarTabela();
+  }
+
+  private carregarTabela() {
     this.get().subscribe((domains: Cliente[]) => {
       if (domains) {
         this.list = domains;
@@ -36,17 +41,41 @@ export class ClienteComponent implements OnInit {
   }
 
   cadastrar(): void {
+    const id = this.form.controls['id'].value;
     const cliente: ClienteModel = this.form.getRawValue();
-    this.post(cliente).subscribe((domain: Cliente) => {
-      if (domain.id) {
-        this.list.push(domain);
-      }
-    });
+    if (id) {
+      this.put(id, cliente).subscribe((domain: Cliente) => {
+        if (domain.id) {
+          this.carregarTabela();
+          this.form.reset();
+        }
+      });
+    } else {
+      this.post(cliente).subscribe((domain: Cliente) => {
+        if (domain.id) {
+          this.list.push(domain);
+          this.form.reset();
+        }
+      });
+    }
+  }
+
+  editar(cliente: Cliente): void {
+    this.form.controls['id'].setValue(cliente.id);
+    this.form.controls['nome'].setValue(cliente.nome);
+    this.form.controls['cpf'].setValue(cliente.documento);
+    this.form.controls['aniver'].setValue(cliente.aniver);
+    this.form.controls['email'].setValue(cliente.email);
   }
 
   private post(model: ClienteModel): Observable<Cliente> {
     const url = 'http://localhost:8080/cliente/cadastrar';
     return this.http.post<Cliente>(url, model);
+  }
+
+  private put(id: string, model: ClienteModel): Observable<Cliente> {
+    const url = 'http://localhost:8080/cliente/alterar/' + id;
+    return this.http.put<Cliente>(url, model);
   }
 
   private get(): Observable<Cliente[]> {
